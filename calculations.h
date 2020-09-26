cf04b44fac6444a21a7c66b5c7aad5d2c24a7f2a
Max <30370890+MaxwellJr@users.noreply.github.com>
2020-09-26 18:38:21 +0300

    Add files

---
+++ calculations.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATIONS_H
+#define CALCULATIONS_H
+
+float determinant(float **matrix, const unsigned int n);
+
+void inverseMatrix(float **matrix, const unsigned int n);
+
+void launchInteraction();
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include "utils.h"
+#include "calculations.h"
+#include <iostream>
+#include <stdexcept>
+
+int main() {
+    try {
+        launchInteraction();
+    } catch (const std::invalid_argument &arg) {
+        std::cerr << arg.what();
+        return 1;
+    }
+    return 0;
+}
